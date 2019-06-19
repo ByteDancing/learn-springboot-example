28ebb7107a296ca450f548bb158062a125385cff
ChengXiang <2393239440@qq.com>
2019-06-19 22:48:28 +0800

    springboot整合RedisTemplate

---
+++ spring-boot-demo-redis/src/main/java/com/union/Application.java
@@ -0,0 +1,19 @@
+package com.union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * description
+ *
+ * @author Mr.Cheng
+ * @date 2019/6/19
+ */
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ spring-boot-demo-redis/src/main/java/com/union/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.union.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author Mr.Cheng
+ * @date 2019/6/19
+ */
+@Configuration
+public class RedisConfig {
+}
